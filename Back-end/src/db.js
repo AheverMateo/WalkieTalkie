3b2d406117c2c732ec80ad926142dda7e4ee1ca2
Mateo <brandonahumada804@gmail.com>
2025-02-18 11:46:34 -0300

    Configuracion de base de datos

--- Back-end/src/db.js
+++ Back-end/src/db.js
@@ -1,12 +1,20 @@
-require('dotenv').config();
-const { Sequelize } = require("sequelize")
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
-const { DB_USER, DB_PASSWORD, DB_HOST, BDD } = process.env
+const { DB_USER, DB_PASSWORD, DB_HOST, BDD, DB_DEPLOY } = process.env;
 
 const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${BDD}`
- )
+  //`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${BDD}`
+  DB_DEPLOY,
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 
-
-
- module.exports = sequelize
+module.exports = sequelize;
