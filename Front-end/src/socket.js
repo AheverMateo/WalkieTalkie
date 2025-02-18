a4e2eb1579f0443979d9b544a8c2727b8dd967fa
Mateo <brandonahumada804@gmail.com>
2025-02-18 15:39:18 -0300

    Configuracion de socket

--- Front-end/src/socket.js
+++ Front-end/src/socket.js
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://localhost:3000", { secure: true });
+const socket = io("https://walkietalkie-40bm.onrender.com", { secure: true });
 console.log(socket);
 
 export default socket;
+
+//https://walkietalkie-40bm.onrender.com
+//https://localhost:3000
