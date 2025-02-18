e0804aec931198c7d3fbfe4ef3676b0b45759db1
Mateo <brandonahumada804@gmail.com>
2025-02-18 12:26:42 -0300

    configuracion http

--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -1,32 +1,21 @@
 const express = require('express');
 const router = require('./routes/routes');
-const https = require('https');
-const path = require('path');
-const fs = require('fs');
-const socketIo = require('socket.io')
+const socketIo = require('socket.io');
 const socketController = require('./controllers/socketController');
 
 const app = express();
+const server = require('http').createServer(app);
 
-
-const credentials = {
-    key: fs.readFileSync(path.join(__dirname, '../certs/server.key')),
-    cert: fs.readFileSync(path.join(__dirname, '../certs/server.cert'))
-}
-
-const httpsServer = https.createServer(credentials, app);
-
-const io = socketIo(httpsServer, {
+const io = socketIo(server, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"],
     }
 });
 
-socketController(io)
+socketController(io);
 
-app.use(express.json())
-
+app.use(express.json());
 app.use('/', router);
 
-module.exports = httpsServer;
+module.exports = server;
